3b032ff6153cf570523e062d901a2f47b91ff47f
k-fujimura-ring <69615045+nyappy15ch@users.noreply.github.com>
2021-05-02 17:58:17 +0900

    formとグリッドのコンポーネント作成

---
+++ src/components/form.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  
+</template>
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  setup() {
+    
+  },
+})
+</script>
+<style scoped>
+
+</style>
