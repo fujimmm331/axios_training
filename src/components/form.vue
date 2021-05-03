884485d5e598f0e809d47aeba53576eadf11fc88
k-fujimura-ring <69615045+nyappy15ch@users.noreply.github.com>
2021-05-03 23:35:40 +0900

    vuetifyを導入しフォームコンポーネントを作成

--- src/components/form.vue
+++ src/components/form.vue
@@ -1,15 +1,22 @@
 <template lang="pug">
-  
+  div#form
+    v-card-title
+      h1 検索する
+    v-card-text
+      v-form
+        v-text-field(
+          prepend-icon="mdi-magnify",
+          label="例）軽井沢 おしゃれ",
+          width="80%")
+      v-card-actions
+        v-btn 検索
 </template>
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
-
+import { defineComponent } from "@vue/composition-api";
 export default defineComponent({
   setup() {
-    
   },
-})
+});
 </script>
 <style scoped>
-
 </style>
--- src/components/table.vue
+++ src/components/table.vue
@@ -1,15 +1,12 @@
 <template lang="pug">
-  
+  div#table
+    p テーブル
 </template>
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api'
-
+import { defineComponent } from "@vue/composition-api";
 export default defineComponent({
-  setup() {
-    
-  },
-})
+  // setup() {
+  // },
+});
 </script>
-<style scoped>
-
-</style>
+<style scoped></style>
