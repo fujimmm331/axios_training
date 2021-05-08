57ad24439caf823cb1b56191e670421e3a0ac087
k-fujimura-ring <69615045+nyappy15ch@users.noreply.github.com>
2021-05-08 22:51:18 +0900

    gridコンポーネントを作成

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template lang="pug">
   v-app
     form-component
+    table-component
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import FormComponent from "./components/form.vue";
+import TableComponent from "./components/table.vue";
 
 
 export default Vue.extend({
   name: "App",
 
   components: {
-    FormComponent
+    FormComponent,
+    TableComponent
   },
 
   data: () => ({
--- src/components/table.vue
+++ src/components/table.vue
@@ -1,6 +1,18 @@
 <template lang="pug">
   div#table
-    p テーブル
+    v-container.grey.lighten-5.mb-6
+      v-row.border_title
+        v-col(:align-self="end") title1
+        v-col(:align-self="center") title2
+        v-col(:align-self="center") title3
+      v-row.border
+        v-col value1
+        v-col value2
+        v-col value3
+      v-row.border
+        v-col value1
+        v-col value2
+        v-col value3
 </template>
 <script lang="ts">
 import { defineComponent } from "@vue/composition-api";
@@ -9,4 +21,11 @@ export default defineComponent({
   // },
 });
 </script>
-<style scoped></style>
+<style scoped>
+.border_title {
+  border-bottom: 1px solid silver;
+}
+.border {
+  border-bottom: 1px solid #e0e0e0;
+}
+</style>
